36b93bdcaaa430d2c5c4054aea35e8a7a8888548
wuhongliang <www.78607532@qq.com>
2017-04-18 13:42:24 +0800

    需求单No.134548 “仙坛模块”【生产通知单】增加“单价”字段，手工录入，“新产品”可选择“是”或者“否”

---
+++ B3Butchery.Web/Pages/TypeIocs/ProductNoticeEditIoc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TSingSoft.WebControls2;
+
+namespace BWP.Web.Pages.TypeIocs
+{
+  public class ProductNoticeEditIoc
+  {
+    public interface BuildDetail 
+    {
+       void Invoke(DFEditGrid grid);
+    }
+  }
+}
