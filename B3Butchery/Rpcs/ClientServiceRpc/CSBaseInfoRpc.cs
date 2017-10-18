611bcc1121e7ec568ec3e9ef314847bd06798ce3
luanhui <1029149336@qq.com>
2017-10-18 17:48:52 +0800

    计数

--- B3Butchery/Rpcs/ClientServiceRpc/CSBaseInfoRpc.cs
+++ B3Butchery/Rpcs/ClientServiceRpc/CSBaseInfoRpc.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using BWP.B3Butchery.BO;
+using BWP.B3Butchery.Rpcs.RpcObject;
 using BWP.B3Frameworks.BO;
 using Forks.EnterpriseServices.BusinessInterfaces;
 using Forks.EnterpriseServices.DomainObjects2;
 using Forks.EnterpriseServices.DomainObjects2.DQuery;
 using Forks.EnterpriseServices.JsonRpc;
+using Forks.EnterpriseServices.SqlDoms;
 using Newtonsoft.Json;
 using TSingSoft.WebPluginFramework;
 
@@ -84,6 +86,143 @@ namespace BWP.B3Butchery.Rpcs.ClientServiceRpc
             entity.ID = (long)reader[0];
             entity.Name = (string)reader[1];
             entity.Spell = (string)reader[2];
+            list.Add(entity);
+          }
+        }
+      }
+      return list;
+    }
+
+    [Rpc(RpcFlags.SkipAuth)]
+    public static List<EmpInfoTable> GetEmpInfo()
+    {
+      var user = new JoinAlias(typeof(WpfUser));
+      var rel = new JoinAlias(typeof(User_Employee));
+      var emp = new JoinAlias(typeof(Employee));
+      var du = new JoinAlias(typeof(DomainUser));
+      var setting = new JoinAlias(typeof(B3FrameworksDomainUserProfile));
+      var query = new DQueryDom(user);
+      query.From.AddJoin(JoinType.Left, new DQDmoSource(rel), DQCondition.EQ(user, "ID", rel, "User_ID"));
+      query.From.AddJoin(JoinType.Left, new DQDmoSource(emp), DQCondition.EQ(rel, "Employee_ID", emp, "ID"));
+      query.From.AddJoin(JoinType.Left, new DQDmoSource(du), DQCondition.And(DQCondition.EQ(rel, "User_ID", du, "User_ID"), DQCondition.EQ(du, "Domain_ID", emp, "Domain_ID")));
+      query.From.AddJoin(JoinType.Left, new DQDmoSource(setting), DQCondition.EQ(du, "ID", setting, "ID"));
+
+
+      query.Columns.Add(DQSelectColumn.Field("ID", user));
+      query.Columns.Add(DQSelectColumn.Field("Name", user));
+      query.Columns.Add(DQSelectColumn.Field("Domain_ID", emp));
+      query.Columns.Add(DQSelectColumn.Field("ID", emp));
+      query.Columns.Add(DQSelectColumn.Field("Name", emp));
+      query.Columns.Add(DQSelectColumn.Field("Department_ID", emp));
+      query.Columns.Add(DQSelectColumn.Field("Department_Name", emp));
+      query.Columns.Add(DQSelectColumn.Field("DefaultAccountingUnit_ID", setting));
+      query.Columns.Add(DQSelectColumn.Field("DefaultAccountingUnit_Name", setting));
+      query.Columns.Add(DQSelectColumn.Field("Remark", emp));
+      query.Where.Conditions.Add(DQCondition.IsNotNull(DQExpression.Field(rel, "Employee_ID")));
+      var list = new List<EmpInfoTable>();
+      using (var context = new TransactionContext())
+      {
+        using (var reader = context.Session.ExecuteReader(query))
+        {
+          while (reader.Read())
+          {
+            var entity = new EmpInfoTable();
+            entity.User_ID = (long)reader[0];
+            entity.User_Name = (string)reader[1];
+            entity.Domain_ID = (long)reader[2];
+            entity.Employee_ID = (long)reader[3];
+            entity.Employee_Name = (string)reader[4];
+            entity.Department_ID = (long?)reader[5];
+            entity.Department_Name = (string)reader[6];
+            entity.AccountingUnit_ID = (long?)reader[7];
+            entity.AccountingUnit_Name = (string)reader[8];
+            entity.Role = (string)reader[9];
+            list.Add(entity);
+          }
+        }
+      }
+      return list;
+    }
+
+    [Rpc(RpcFlags.SkipAuth)]
+    public static List<WpfUser> GetWpfUser()
+    {
+      var query = new DQueryDom(new JoinAlias(typeof(WpfUser)));
+      query.Columns.Add(DQSelectColumn.Field("ID"));
+      query.Columns.Add(DQSelectColumn.Field("Name"));
+      query.Columns.Add(DQSelectColumn.Field("Stopped"));
+      query.Columns.Add(DQSelectColumn.Field("Password"));
+      query.Columns.Add(DQSelectColumn.Field("RoleSchema"));
+
+      var list = new List<WpfUser>();
+      using (var context = new TransactionContext())
+      {
+        using (var reader = context.Session.ExecuteReader(query))
+        {
+          while (reader.Read())
+          {
+            var entity = new WpfUser();
+            entity.ID = (long)reader[0];
+            entity.Name = (string)reader[1];
+            entity.Stopped = (bool)reader[2];
+            entity.Password = (byte[])reader[3];
+            entity.RoleSchema = (string)reader[4];
+            list.Add(entity);
+          }
+        }
+      }
+      return list;
+    }
+
+    [Rpc(RpcFlags.SkipAuth)]
+    public static List<User_Employee> GetUserEmployee()
+    {
+      var query = new DQueryDom(new JoinAlias(typeof(User_Employee)));
+      query.Columns.Add(DQSelectColumn.Field("Employee_ID"));
+      query.Columns.Add(DQSelectColumn.Field("User_ID"));
+
+      var list = new List<User_Employee>();
+      using (var context = new TransactionContext())
+      {
+        using (var reader = context.Session.ExecuteReader(query))
+        {
+          while (reader.Read())
+          {
+            var entity = new User_Employee();
+            entity.Employee_ID = (long)reader[0];
+            entity.User_ID = (long)reader[1];
+            list.Add(entity);
+          }
+        }
+      }
+      return list;
+    }
+
+    [Rpc(RpcFlags.SkipAuth)]
+    public static List<Employee> GetEmployee()
+    {
+      var query = new DQueryDom(new JoinAlias(typeof(Employee)));
+      query.Columns.Add(DQSelectColumn.Field("ID"));
+      query.Columns.Add(DQSelectColumn.Field("Name"));
+      query.Columns.Add(DQSelectColumn.Field("Code"));
+      query.Columns.Add(DQSelectColumn.Field("Stopped"));
+      query.Columns.Add(DQSelectColumn.Field("Domain_ID"));
+      query.Columns.Add(DQSelectColumn.Field("Spell"));
+
+      var list = new List<Employee>();
+      using (var context = new TransactionContext())
+      {
+        using (var reader = context.Session.ExecuteReader(query))
+        {
+          while (reader.Read())
+          {
+            var entity = new Employee();
+            entity.ID = (long)reader[0];
+            entity.Name = (string)reader[1];
+            entity.Code = (string)reader[2];
+            entity.Stopped = (bool)reader[3];
+            entity.Domain_ID = (long)reader[4];
+            entity.Spell = (string)reader[5];
             list.Add(entity);
           }
         }
---
+++ B3Butchery/Rpcs/RpcObject/EmpInfoTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Forks.EnterpriseServices.JsonRpc;
+
+namespace BWP.B3Butchery.Rpcs.RpcObject
+{
+  [RpcObject]
+  public class EmpInfoTable
+  {
+    public long User_ID { get; set; }
+    public string User_Name { get; set; }
+    public long Domain_ID { get; set; }
+    public long Employee_ID { get; set; }
+    public string Employee_Name { get; set; }
+    public long? AccountingUnit_ID { get; set; }
+    public string AccountingUnit_Name { get; set; }
+    public long? Department_ID { get; set; }
+    public string Department_Name { get; set; }
+
+    public string Role { get; set; }
+  }
+}
