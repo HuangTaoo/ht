304a9ddac33b98fe775c89f5069ad27224699d3a
wushukun <18860363876@163.com>
2017-11-05 18:28:24 +0800

    增加

---
+++ B3Butchery/BL/BaseInfo/HandoverRecord_/HandoverRecordBL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BWP.B3Butchery.BO.BaseInfo;
+using BWP.B3Frameworks.BL;
+using Forks.EnterpriseServices;
+using Forks.EnterpriseServices.BusinessInterfaces;
+
+namespace BWP.B3Butchery.BL.BaseInfo.HandoverRecord_
+{
+
+    [BusinessInterface(typeof(HandoverRecordBL))]
+    [LogicName("交接记录")]
+    public interface IHandoverRecordBL : IBaseBL<HandoverRecord>
+    { }
+    public class HandoverRecordBL : BaseBL<HandoverRecord>, IHandoverRecordBL
+    {
+
+    }
+}
---
+++ B3Butchery/BO/BaseInfo/HandoverRecord_/HandoverRecord.cs
@@ -0,0 +1,74 @@
+﻿using BWP.B3Frameworks.BO;
+using Forks.EnterpriseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Forks.EnterpriseServices.DomainObjects2;
+
+namespace BWP.B3Butchery.BO.BaseInfo
+{
+    [Serializable, LogicName("交接记录")]
+    public class HandoverRecord : Base
+    {
+        /// <summary>
+        /// 单据创建人用户名
+        /// </summary>
+        public string CreateUser_Name { get; set; }
+        public DateTime? ModifyTime { get; set; }
+        public bool DeleteState { get; set; }
+
+        [LogicName("计数名称")]
+        public long? CalculateGoods_ID { get; set; }
+        [LogicName("计划号")]
+        public string PlanNumber { get; set; }
+
+        [LogicName("计数名称")]
+        public string CalculateGoods_Name { get; set; }
+
+        [LogicName("计数规格")]
+        public string CalculateSpec_Name { get; set; }
+
+        [LogicName("盘数")]//辅数量
+        public decimal? InputNumber { get; set; }
+
+        [LogicName("主辅换算主单位比例")]
+        public decimal? MainUnitRatio { get; set; }
+
+        [LogicName("主辅II换算主单位比例")]
+        public decimal? SecondUnitII_MainUnitRatio { get; set; }
+
+
+        [LogicName("辅单位II数量")]
+        public decimal? SecondIINumber { get; set; }
+
+        //主数量=辅数量ii* 主辅ii换算主单位比例
+
+        //辅数量=主数量/主辅换算主单位比例
+
+        [LogicName("重量")]//主数量
+        public decimal? Weight
+        {
+            get;
+            set;
+        }
+
+        [LogicName("显示名称")]
+        public string DisplayName
+        {
+            get;
+            set;
+        }
+
+        [LogicName("是否辅单位Ⅱ")]
+        public bool? IsSecondⅡ { get; set; }
+
+
+        [LogicName("存货ID")]
+        public long? Goods_ID { get; set; }
+
+        [LogicName("标识")]//记录哪台电脑
+        public string BiaoShi { get; set; }
+
+    }
+}
