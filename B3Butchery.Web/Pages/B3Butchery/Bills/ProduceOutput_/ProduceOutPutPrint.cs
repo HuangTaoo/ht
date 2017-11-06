a4c50e7aab97f44b090f894a8164630912d6eb14
wushukun <18860363876@163.com>
2017-11-06 19:30:55 +0800

    提交

--- B3Butchery.Web/Pages/B3Butchery/Bills/ProduceOutput_/ProduceOutPutPrint.cs
+++ B3Butchery.Web/Pages/B3Butchery/Bills/ProduceOutput_/ProduceOutPutPrint.cs
@@ -13,8 +13,22 @@ namespace BWP.Web.Pages.B3Butchery.Bills.ProduceOutput_
     {
         protected override void AddParameters(IDictionary<string, object> dic)
         {
-            dic.Add("$Details", Dmo.Details);
+            var details = new List<ProduceOutput_Detail>();
+            var group = Dmo.Details.GroupBy(x => x.CalculateCatalog_ID);
+            foreach (var one in group)
+            {
+                foreach (var de in one)
+                {
+                    details.Add(de);
+                }
+            }
+            dic.Add("$Details", details);
             dic.Add("$DetailType", typeof(ProduceOutput_Detail));
         }
+
+
+
+
+
     }
 }
