3ea5b997defd67c750b95360d81439f2f0df837a
wushukun <18860363876@163.com>
2018-01-09 13:57:38 +0800

    修改 车间包装配置单 的列表界面

--- B3Butchery.Web/Pages/B3Butchery/BaseInfos/WorkShopCountConfig_/WorkShopCountConfigList.cs
+++ B3Butchery.Web/Pages/B3Butchery/BaseInfos/WorkShopCountConfig_/WorkShopCountConfigList.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TSingSoft.WebControls2;
 
 namespace BWP.Web.Pages.B3Butchery.BaseInfos.WorkShopCountConfig_
 {
@@ -13,5 +14,27 @@ namespace BWP.Web.Pages.B3Butchery.BaseInfos.WorkShopCountConfig_
     class WorkShopCountConfigList : DomainBaseInfoListPage<WorkShopCountConfig, IWorkShopCountConfigBL>
     {
 
+    protected override void AddQueryControls(VLayoutPanel vPanel)
+    {
+
+
+      vPanel.Add(CreateDefaultBaseInfoQueryControls((panel, config) =>
+      {
+        config.Add("WorkshopCategory_ID");
+        config.Add("No");
+      }));
     }
+
+    protected override void AddDFBrowseGridColumn(DFBrowseGrid grid, string field)
+    {
+
+      base.AddDFBrowseGridColumn(grid, field);
+      if (field == "Name")
+      {
+        AddDFBrowseGridColumn(grid, "WorkshopCategory_Name");
+        AddDFBrowseGridColumn(grid, "No");
+      }
+    }
+
+  }
 }
