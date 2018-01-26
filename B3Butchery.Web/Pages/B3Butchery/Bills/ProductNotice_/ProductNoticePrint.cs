d1d5be5a7f9bcf6facacb715323252712853f699
wugang <425674808@qq.com>
2018-01-26 15:17:03 +0800

    增加排序

--- B3Butchery.Web/Pages/B3Butchery/Bills/ProductNotice_/ProductNoticePrint.cs
+++ B3Butchery.Web/Pages/B3Butchery/Bills/ProductNotice_/ProductNoticePrint.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 using BWP.B3Butchery.BL;
 using BWP.B3Butchery.BO;
+using System.Linq;
 
 namespace BWP.Web.Pages.B3Butchery.Bills.ProductNotice_ {
   class ProductNoticePrint : DomainTemplatePrintPage<ProductNotice, IProductNoticeBL> {
     protected override void AddParameters(IDictionary<string, object> dic) {
       dic.Add("$Details", Dmo.Details);
+
+      dic.Add("$OrderByDetails", Dmo.Details.OrderBy(x => x.OrderByID).ToList());
       dic.Add("$DetailType", typeof(ProductNotice_Detail));
     }
   }
